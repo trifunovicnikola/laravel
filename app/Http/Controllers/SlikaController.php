fbf2a82d2a9ead636976ae9a3eff8d69695361b5
trifunovicnikola <nikola.trifunovic@ict-investment.me>
2021-04-22 10:34:21 +0200

    cdsac

--- app/Http/Controllers/SlikaController.php
+++ app/Http/Controllers/SlikaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SlikaController extends Controller
 {
@@ -13,6 +14,7 @@ class SlikaController extends Controller
     }
     public function dajSliku($telefon_id)
     {
-        return photo::find($telefon_id);
+//        return photo::find($telefon_id);
+        return DB::select('SELECT * FROM PHOTOS WHERE telefon_id='.$telefon_id);
     }
 }
