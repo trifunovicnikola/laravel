27572912d4c93889c985d7d9a0531ab6a680e0c7
trifunovicnikola <nikola.trifunovic@ict-investment.me>
2021-04-22 10:44:37 +0200

    no message

--- app/Http/Controllers/fileController.php
+++ app/Http/Controllers/fileController.php
@@ -9,52 +9,51 @@ class fileController extends Controller
 public function  file(Request $request)
 {
     $photo = new photo;
-    if ($request->hasFile('slika1')) {
-$completeFileName=$request->file('slika1')->getClientOriginalName();
+    if ($request->hasFile('slika')) {
+$completeFileName=$request->file('slika')->getClientOriginalName();
 $filenameonly = pathinfo($completeFileName,PATHINFO_FILENAME);
-$extension = $request->file('slika1')->getClientOriginalExtension();
+$extension = $request->file('slika')->getClientOriginalExtension();
 $compPic =str_replace(' ','_',$filenameonly).'_'.rand() .'_'.time(). '.'.
     $extension;
-$path=$request->file('slika1')->storeAs('public/slike',$compPic);
+$path=$request->file('slika')->storeAs('public/slike',$compPic);
 
-if ($request->hasFile('slika2')) {
-            $completeFileName1=$request->file('slika2')->getClientOriginalName();
-            $filenameonly1 = pathinfo($completeFileName1,PATHINFO_FILENAME);
-            $extension1 = $request->file('slika2')->getClientOriginalExtension();
-            $compPic1 =str_replace(' ','_',$filenameonly1).'_'.rand() .'_'.time(). '.'.
-                $extension1;
-            $path=$request->file('slika2')->storeAs('public/slike',$compPic1);
-
-    if ($request->hasFile('slika3')) {
-        $completeFileName2=$request->file('slika3')->getClientOriginalName();
-        $filenameonly2 = pathinfo($completeFileName2,PATHINFO_FILENAME);
-        $extension2 = $request->file('slika3')->getClientOriginalExtension();
-        $compPic2 =str_replace(' ','_',$filenameonly2).'_'.rand() .'_'.time(). '.'.
-            $extension2;
-        $path=$request->file('slika3')->storeAs('public/slike',$compPic2);
-
-
-
-
+//if ($request->hasFile('slika2')) {
+//            $completeFileName1=$request->file('slika2')->getClientOriginalName();
+//            $filenameonly1 = pathinfo($completeFileName1,PATHINFO_FILENAME);
+//            $extension1 = $request->file('slika2')->getClientOriginalExtension();
+//            $compPic1 =str_replace(' ','_',$filenameonly1).'_'.rand() .'_'.time(). '.'.
+//                $extension1;
+//            $path=$request->file('slika2')->storeAs('public/slike',$compPic1);
+//
+//    if ($request->hasFile('slika3')) {
+//        $completeFileName2=$request->file('slika3')->getClientOriginalName();
+//        $filenameonly2 = pathinfo($completeFileName2,PATHINFO_FILENAME);
+//        $extension2 = $request->file('slika3')->getClientOriginalExtension();
+//        $compPic2 =str_replace(' ','_',$filenameonly2).'_'.rand() .'_'.time(). '.'.
+//            $extension2;
+//        $path=$request->file('slika3')->storeAs('public/slike',$compPic2);
+//
+//
 
 
 
 
-   $photo->slika1=$compPic;
-    $photo->slika2=$compPic1;
-        $photo->slika3=$compPic2;
-        $photo->save();
-
-}}}
 
 
-    if ($photo->save()){
+   $photo->slika=$compPic;
+   $photo->telefon_id = $request->telefon_id;
+   $photo->save();
+
+}
+
+
+
+if ($photo->save()){
 //        return ['status'=>true,'message'=>'Sacuvan '];
         return $photo;
 
     }
 else{
     return ['status'=>true,'message'=>'Nije dobro '];
-
 }
 }}
