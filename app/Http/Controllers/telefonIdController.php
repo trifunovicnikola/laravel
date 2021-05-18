2447ea8bb64d7cbdecad0807c2971250fc9edd23
PavleCvorovic <pavle.cvorovic@ict-investment.me>
2021-05-18 11:37:54 +0200

    no message

--- app/Http/Controllers/telefonIdController.php
+++ app/Http/Controllers/telefonIdController.php
@@ -53,7 +53,7 @@ class telefonIdController
     public function dajTelefone()
     {
 
-return DB::select('select tel.id as id, tel.model as model, marks.marka_naziv as naziv, tel.cijena as cijena , photos.slika as slika ,tel.prodavac as prodavac ,tel.kontakt as kontakt
+return DB::select('select tel.stanje as stanje,tel.id as id, tel.model as model, marks.marka_naziv as naziv, tel.cijena as cijena , photos.slika as slika ,tel.prodavac as prodavac ,tel.kontakt as kontakt
 FROM telefons as tel
 join marks on (marks.id = tel.mark_id)
 join photos on (photos.telefon_id = tel.id)
